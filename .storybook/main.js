4081a8c6c04a03c0606400137b1691d8041b4672
Luiz Felipe Ribeiro Baroncello <luizfelipe_rv97@hotmail.com>
2020-10-08 21:17:41 -0300

    Absolute paths on storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,11 @@
 module.exports = {
-  "stories": [
-    "../src/components/**/stories.tsx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../src/components/**/stories.tsx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  "addons": [
-    "@storybook/addon-essentials"
-  ]
+  addons: ['@storybook/addon-essentials'],
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+    return config
+  }
 }
